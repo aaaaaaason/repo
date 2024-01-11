b44375d5ce67a4a922b0f0e5cedb1802986999b9
andychen <aaaaaaason@gmail.com>
2024-01-11 09:00:07 +0800

    update tmux.conf

--- shell/dev/tmux.sh
+++ shell/dev/tmux.sh
@@ -4,11 +4,13 @@ if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   echo "git clone tmux plugin manager..."
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-  cat << EOF >> ~/.tmux.conf
+  cat <<EOF >>~/.tmux.conf
 # List of plugins
 set -g @plugin 'tmux-plugins/tpm'
 set -g @plugin 'tmux-plugins/tmux-sensible'
 set -g @plugin 'tmux-plugins/tmux-resurrect'
+set -g @plugin 'tmux-plugins/tmux-open'
+set -g @plugin 'tmux-plugins/tmux-yank'
 
 unbind-key C-b
 
@@ -16,10 +18,18 @@ set-option -g prefix C-a
 
 bind-key C-a send-prefix
 
+set -g mode-mouse on
+
+set-window-option -g mode-keys vi
+
+# Use v to trigger selection
+bind-key -T copy-mode-vi v send-keys -X begin-selection
+
+# Use y to yank current selection
+bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
 # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
 run -b '~/.tmux/plugins/tpm/tpm'
 
 EOF
 fi
-
-
