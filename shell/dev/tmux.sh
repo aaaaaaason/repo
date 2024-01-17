6c949072a65cbd2b279a2c801741f1252ff8cf94
aaaaaaason <aaaaaaason@gmail.com>
2024-01-17 23:08:17 +0800

    add more tmux plugins

--- shell/dev/tmux.sh
+++ shell/dev/tmux.sh
@@ -9,8 +9,12 @@ if [[ ! -d ~/.tmux/plugins/tpm ]]; then
 set -g @plugin 'tmux-plugins/tpm'
 set -g @plugin 'tmux-plugins/tmux-sensible'
 set -g @plugin 'tmux-plugins/tmux-resurrect'
+set -g @plugin 'tmux-plugins/tmux-continuum'
 set -g @plugin 'tmux-plugins/tmux-open'
+set -g @plugin 'tmux-plugins/tmux-copycat'
 set -g @plugin 'tmux-plugins/tmux-yank'
+set -g @plugin 'tmux-plugins/tmux-pain-control'
+set -g @plugin 'christoomey/vim-tmux-navigator'
 
 unbind-key C-b
 
